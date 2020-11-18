def96609c84f99dba71313eddb27b09ebc148a4e
JC <jcal.d.as.1.9.99+vivasgithub@gmail.com>
2020-11-18 14:02:42 +0100

    Initial commit.

---
+++ T4E3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace T4E3
+{
+    class Program
+    {
+        private static readonly object l = new object();
+        private delegate int UpdateVarOp(int n);
+
+        static void Main(string[] args)
+        {
+            int n = 0;
+            Thread t1 = new Thread(() => UpdateVar(ref n, m => m + 1));
+            Thread t2 = new Thread(() => UpdateVar(ref n, m => m - 1));
+
+            t1.Start();
+            t2.Start();
+        }
+
+        private static void UpdateVar(ref int n, UpdateVarOp op)
+        {
+            Func<int, bool> stopCond = m => Math.Abs(m) == 1000;
+
+            while (!stopCond(n))
+            {
+                lock (l)
+                {
+                    if (!stopCond(n))
+                    {
+                        n = op(n);
+                        Console.SetCursorPosition(1, 1);
+                        Console.Write("{0,-5}", n);
+                    }
+                }
+            }
+        }
+    }
+}
